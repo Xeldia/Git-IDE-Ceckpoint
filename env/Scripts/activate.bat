dd3fe68b2db21269b5b6bbb22f124bb23024ff9c
Andre <andrecodilla03@gmail.com>
2025-10-01 01:22:39 +0800

    Collect static files for Render deployment

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=Z:\L23Y15W08\Pyth_IDE\env
+set VIRTUAL_ENV=C:\Users\andre\OneDrive\Documents\Projects\Pyth_IDE\env
 
 if not defined PROMPT set PROMPT=$P$G
 
