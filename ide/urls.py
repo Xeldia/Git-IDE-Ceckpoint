de260de1eee0e30a19d7fd8153c0d35b4bf59af7
Andre <andrecodilla03@gmail.com>
2025-10-07 18:34:42 +0800

    Your commit message here

--- ide/urls.py
+++ ide/urls.py
@@ -4,8 +4,8 @@ from .views import chat_view
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("python-ide/", views.python_ide, name="python_ide"),
-    path("java-ide/", views.java_ide, name="java_ide"),
+    path("python/", views.python_ide, name="python_ide"),
+    path("java/", views.java_ide, name="java_ide"),
     path('execute/', views.execute_java_code, name='execute_java'),
 ]
 
